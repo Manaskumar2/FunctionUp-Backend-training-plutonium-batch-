49609e069b1ac5eaecd10ad8cbcc07b2d3b3f1fc
Manas kumar Jena <you@example.com>
2022-08-09 22:20:41 +0530

    my W4D2 assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const myHelper = require('../util/helper')
 const underscore = require('underscore')
 
 const router = express.Router();
+const app=express.Router()
 
 router.get('/test-me', function (req, res) {
     myHelper.printDate()
@@ -102,6 +103,48 @@ router.get("/films/:filmId", function(req, res){
        //if there is no match give an error response
        res.send("The film id doesn't match any movie")
 })
+   // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7] : 4 is missing
+ // Your route code will look like this
+ app.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7] //4 ->7(7+1)/2=7(8)/2=>7*4=28 -24=4
+    let bag =0;//all total number 24
+    for(let i in arr){
+        bag=bag+arr[i]
+
+    }
+    let num=arr.pop()
+    let sumOfNum=num*(num +1)/2;//all total =28
+    let missingNumber=sumOfNum-bag //28-24=4
+
+    ///LOGIC WILL GO HERE 
+    res.send(  { data: missingNumber  }  );
+});
+
+
+// -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33, 34, 35, 37, 38]: 36 is missing
+// Your route code will look like this
+  app.get("/sol2", function (req, res) {
+        //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+        let arr= [33, 34, 35, 37, 38]// 36 // 6*(33+38)/2=6*71*/2=213
+        let n=arr.length+1
+        let bag =0;//33+34+35+37+38=177
+    for(let i in arr){
+        bag=bag+arr[i]
+
+    }
+    let firstNumber=arr[0]
+    let lastNumber=arr.pop()
+    let sumOfNum=n*(firstNumber+lastNumber)/2 //all total 213
+    let missingNumber=sumOfNum-bag //213-177 =36
+
+        ///LOGIC WILL GO HERE 
+
+        res.send(  { data: missingNumber  }  );
+});
+
+
 
 module.exports = router;
+module.exports =app;
 // adding this comment for no reason
