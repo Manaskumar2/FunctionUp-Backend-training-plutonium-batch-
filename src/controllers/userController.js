6a57e91e33e258b003544fafaec019f84c7858ca
Manas kumar Jena <you@example.com>
2022-08-13 19:38:37 +0530

    mongoose schema

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const UserModel= require("../models/userModel")
+const bookSchema =require("../bookschema/bookschem")
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -10,6 +11,17 @@ const getUsersData= async function (req, res) {
     let allUsers= await UserModel.find()
     res.send({msg: allUsers})
 }
+const createNewBook= async function(req,res){
+    let newBook=req.body
+    let newBookCreate= await bookSchema.create(newBook)
+    res.send({msg:newBookCreate})
+}
+const listOfAllBook =async function(req,res){
+    let allBooks =await bookSchema.find()
+    res.send({msg: allBooks})
+}
 
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
+module.exports.createNewBook= createNewBook
+module.exports.listOfAllBook=listOfAllBook
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
@@ -10,5 +11,7 @@ router.get("/test-me", function (req, res) {
 router.post("/createUser", UserController.createUser  )
 
 router.get("/getUsersData", UserController.getUsersData)
+router.post("/createNewBook",UserController.createNewBook)
+router.get("/booklist",UserController.listOfAllBook)
 
 module.exports = router;
