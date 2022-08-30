904ba6d06f0f04c595c0f515e64fa2e45c221655
Manas kumar Jena <you@example.com>
2022-08-30 22:30:27 +0530

    try-catch

--- src/controllers/NewUsercontroller.js
+++ src/controllers/NewUsercontroller.js
@@ -7,15 +7,24 @@ const jwt= require("jsonwebtoken")
 
 
 const createUser= async (req,res)=>{
-    let userDetails= req.body
-    let usersData= await user1Model.create(userDetails)
-    res.send({msg:usersData})
+    try {
+       let  userDetails =req.body
+        let usersData= await user1Model.create(userDetails)
+    res.status(201).send({msg:usersData})
+        
+    } catch (error) {
+       
+        res.status(500).send({error:error.message})
+        
+    }
+    
 }
 const loginUser = async  (req, res)=> {
+    try{
     let { emailId } = req.body
     let { password } = req.body
-    let userCheck = await user1Model.findOne({ emailId: emailId, password: password })
-    if (!userCheck) { return res.send("Incorrect userID or password.") }
+    let userCheck = await user1Model.findOne({ emailId: emailId, password: password})
+    if (!userCheck) { return res.status(404).send("Incorrect userID or password.") }
 
     let token = jwt.sign(
         {
@@ -24,27 +33,46 @@ const loginUser = async  (req, res)=> {
         },
         "hi there"
     )
-    res.send({ status: true, token: token })
+    res.status(200).send({ status: true, token: token })
+    }
+    catch(err){
+        res.status(500).send({error:err.message})
+    }
 }
 const userDetails = async (req,res)=>{
+    try{
     const valid=req.params.userId
     const userDetail= await user1Model.findById(valid)
 
 
 
-    res.send({msg:userDetail})
+    res.status(200).send({msg:userDetail})
+    }
+    catch(err){
+        res.status(500).send({error:err.message})
+    }
 
 }
-const updateDetails = async function (req, res) {
+const updateDetails = async (req, res)=> {
+    try{
     let add = req.body
     let userId=req.params.userId
     let update = await user1Model.findByIdAndUpdate(userId, add, { new: true })
-    res.send({msg:update})
+    res.status(201).send({msg:update})
+    }
+    catch(err){
+        res.status(500).send({error:err.message})
+    }
 }
-const deleteDetails = async function (req, res) {
+const deleteDetails = async (req, res)=> {
+    try{
     let userId=req.params.userId
     let update = await user1Model.findByIdAndUpdate(userId , { isDeleted: true }, { new: true })
-    res.send(update)
+    res.status(200).send(update)
+    }
+    catch(err){
+        res.status(500).send({error:err.message})
+    }
 }
 module.exports.createUser=createUser
 module.exports.loginUser=loginUser
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,24 +5,41 @@ const jwt = require("jsonwebtoken")
 const authenticate = function(req, res, next) {
     //check the token in request header
     //validate this token
-
+ try{      
+    
  let token = req.headers["x-auth-token"]
-    if (!token) { return res.send("Header is missing") }
+    if (!token) { return res.status(404).send("Header is missing") }
     let decodedToken = jwt.verify(token, "hi there")
-    if (!decodedToken) { return res.send("Not a valid token") }
+    if (!decodedToken) { return res.status(404).send("Not a valid token") }
     req.decodedToken=decodedToken
     next()
 }
+catch(error){
+res.status(500).send({error:error.message})
+}
+
+}
+
+
 const authorise = async function(req, res, next) {
+
     // comapre the logged in user's id and the id in request
+try{
 let userId= req.params.userId
  let check = await user1Model.findById(userId)
-    if (!check) { return res.send("No such user exist") }
+    if (!check) { return res.status(404).send("No such user exist") }
 let ModifiedUserId=req.decodedToken.userID
-if(userId !== ModifiedUserId) return res.send({status:false,msg:"this is not s valid user"})
+if(userId !== ModifiedUserId) return res.status(403).send({status:false,msg:"this is not s valid user"})
 
 
     next()
+}
+
+catch(err){
+
+res.status(500).send({errer:err.message})
+
+}
 }
 module.exports.authenticate=authenticate
 module.exports.authorise=authorise
